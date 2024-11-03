923231998f7bfb7123ba5f8c0c039af201c42ae7
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2024-11-03 22:41:52 +0330

    created react-demos app

---
+++ front-end/React/react-demos/src/Components/JSXRules.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSX Rules</h1>
+      <p className="text">Hello from a jsx paragraph.</p>
+    </div>
+  );
+};
+
+export default JSXRules;
---
+++ front-end/React/react-demos/src/Components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main>
+      <h2>Main Content</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        quibusdam quod praesentium ipsum suscipit alias. Numquam asperiores, est
+        perferendis unde officia inventore vel! Recusandae cum iste nisi aliquam
+        officiis fugit?
+      </p>
+    </main>
+  );
+};
+
+export default MainContent;
